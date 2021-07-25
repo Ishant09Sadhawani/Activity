c3b8dcee98e99d35fa54d96ec30e6d1cbbcc1aa0
Ishant09Sadhawani <73270232+Ishant09Sadhawani@users.noreply.github.com>
2021-07-25 22:36:38 +0530

    Update organize.js

--- comands/organize.js
+++ comands/organize.js
@@ -1,7 +1,50 @@
+let fs = require("fs");
+let path = require("path");
 
-function fn(path){
-    console.log("organize command executed with path "+path);
+let types = {
+    media: ["mp4", "mkv"],
+    archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
+    documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex'],
+    app: ['exe', 'dmg', 'pkg', "deb"]
 }
+
+function fn(path1){
+    
+    let folderCont = fs.readdirSync(path1);                  
+
+    let mainFolderPath = path.join(path1,"organize");           
+    fs.mkdirSync(mainFolderPath);                               
+
+    for(let i = 0 ; i < folderCont.length ; i++){            
+        let extension = path.extname(folderCont[i]);
+        let ext = extension.split(".")[1];
+        // console.log(extension);
+        // console.log(ext);
+        let folderName = "other";
+
+        for(key in types){                                      
+            for(let j = 0 ; j < types[key].length ; j++){       
+                if(ext == types[key][j]){                       
+                    folderName = key;
+                    break;
+                } 
+            }
+        }
+
+        let typesPath = path.join(mainFolderPath,folderName);
+
+        let doesExist = fs.existsSync(typesPath);
+        
+        if(!doesExist){                                         
+                fs.mkdirSync(typesPath);                        
+        } 
+
+        let srcPath = path.join(path1,folderCont[i]);                        
+        let destPath = path.join(mainFolderPath,folderName,folderCont[i]);   
+        fs.copyFileSync(srcPath,destPath);
+    }
+}
+
 module.exports = {
-    fxn2: fn
+    fxn2 : fn
 }
